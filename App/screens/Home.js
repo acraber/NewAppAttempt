678f61a05a948f1ae308336e035b50693426b84d
acraber2@gmail.com <Setup2getup!hub>
2020-10-19 00:35:19 -0700

    Fixed lint settings. BLANK MAIN VERSION 1

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -2,15 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            < StatusBar style="auto" />
-        </View>
-    );
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -19,5 +10,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+export default function App() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+}
+
+
 
 // 4* copy this from App.js or just copy this file
